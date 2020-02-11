ef42da99db56ec0ec0e805da5e75821d2fd6dedb
Shayan Javadi <shayanjavadi1375@gmail.com>
2020-02-10 23:43:14 -0600

    Update Plugin.swift

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,7 +1,43 @@
 import Foundation
 import Capacitor
 import AuthenticationServices
+import CryptoKit
 
+// Unhashed nonce.
+fileprivate var currentNonce: String?
+
+// Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+private func randomNonceString(length: Int = 32) -> String {
+  precondition(length > 0)
+  let charset: Array<Character> =
+      Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+  var result = ""
+  var remainingLength = length
+
+  while remainingLength > 0 {
+    let randoms: [UInt8] = (0 ..< 16).map { _ in
+      var random: UInt8 = 0
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+      if errorCode != errSecSuccess {
+        fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+      }
+      return random
+    }
+
+    randoms.forEach { random in
+      if remainingLength == 0 {
+        return
+      }
+
+      if random < charset.count {
+        result.append(charset[Int(random)])
+        remainingLength -= 1
+      }
+    }
+  }
+
+  return result
+}
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
@@ -14,17 +50,33 @@ public class SignInWithApple: CAPPlugin {
         self.call = call
 
         if #available(iOS 13.0, *) {
+           let nonce = randomNonceString()
+            currentNonce = nonce
             let appleIDProvider = ASAuthorizationAppleIDProvider()
             let request = appleIDProvider.createRequest()
             request.requestedScopes = [.fullName, .email]
+            request.nonce = sha256(nonce)
+
 
             let authorizationController = ASAuthorizationController(authorizationRequests: [request])
             authorizationController.delegate = self
+
             authorizationController.performRequests()
         } else {
             call.reject("Sign in with Apple is available on iOS 13.0+ only.")
         }
     }
+}
+
+@available(iOS 13, *)
+private func sha256(_ input: String) -> String {
+  let inputData = Data(input.utf8)
+  let hashedData = SHA256.hash(data: inputData)
+  let hashString = hashedData.compactMap {
+    return String(format: "%02x", $0)
+  }.joined()
+
+  return hashString
 }
 
 @available(iOS 13.0, *)
@@ -35,15 +87,34 @@ extension SignInWithApple: ASAuthorizationControllerDelegate {
 
             return
         }
-
+         guard let nonce = currentNonce else {
+          fatalError("Invalid state: A login callback was received, but no login request was sent.")
+        }
+        guard let appleIDToken = appleIDCredential.identityToken else {
+          print("Unable to fetch identity appleIDToken")
+          return
+        }
+        guard let appleAuthorizationCode = appleIDCredential.authorizationCode else {
+          print("Unable to fetch identity appleAuthorizationCode")
+          return
+        }
+        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+          print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+          return
+        }
+        guard let accessTokenString = String(data: appleAuthorizationCode, encoding: .utf8) else {
+          print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+          return
+        }
         let result = [
             "response": [
                 "user": appleIDCredential.user,
                 "email": appleIDCredential.email,
                 "givenName": appleIDCredential.fullName?.givenName,
                 "familyName": appleIDCredential.fullName?.familyName,
-                "identityToken": String(data: appleIDCredential.identityToken!, encoding: .utf8),
-                "authorizationCode": String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
+                "nonce": nonce,
+                "accessTokenString":accessTokenString,
+                "idTokenString": idTokenString
             ]
         ]
 
